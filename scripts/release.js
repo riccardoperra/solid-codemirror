ab5745dc203e76bdf05e5c13dffdd6d97d24bef1
Riccardo Perra <riccardo.perra@icloud.com>
2022-02-05 16:17:55 +0100

    chore: fix publish ci

---
+++ scripts/release.js
@@ -0,0 +1,6 @@
+import * as fs from 'fs';
+
+const packageJson = fs.readFileSync('./package.json', 'utf8');
+const {scripts, ...other} = JSON.parse(packageJson);
+fs.writeFileSync('./dist/package.json', JSON.stringify(other, null, 2));
+
