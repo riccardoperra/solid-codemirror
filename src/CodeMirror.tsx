64460c7299856adfb972d7e6ab5f7e918cf167ff
Riccardo Perra <riccardo.perra@icloud.com>
2022-02-05 21:03:12 +0100

    refactor: extract codeMirror splitProps into utility function

--- src/CodeMirror.tsx
+++ src/CodeMirror.tsx
@@ -1,63 +1,32 @@
-import {Component, createEffect, mergeProps, onCleanup, splitProps} from 'solid-js';
-import {Extension} from '@codemirror/state';
+import {Component, createEffect, onCleanup} from 'solid-js';
 import {CodeMirrorComponentProps} from './types/codeMirrorProps';
 import {createCodeMirror} from './createCodeMirror';
+import {extractCodeMirrorProps} from "./utils/extractCodeMirrorProps";
 
 export const CodeMirror: Component<CodeMirrorComponentProps> = props => {
-  const [pickedProps, others] = splitProps(props, [
-    'className',
-    'value',
-    'selection',
-    'onChange',
-    'onUpdate',
-    'autoFocus',
-    'height',
-    'minHeight',
-    'maxHeight',
-    'width',
-    'minWidth',
-    'maxWidth',
-    'basicSetup',
-    'placeholder',
-    'editable',
-    'root',
-  ]);
-
-  const computedProps = mergeProps(
-    {
-      extensions: [] as Extension[],
-      height: '',
-      minHeight: '',
-      maxHeight: '',
-      placeHolder: '',
-      width: '',
-      minWidth: '',
-      maxWidth: '',
-      editable: true,
-      indentWithTab: true,
-      basicSetup: true
-    },
-    pickedProps
-  );
-
   let editor!: HTMLDivElement;
+  const {options, htmlAttrs} = extractCodeMirrorProps(props);
 
-  const {view, setOptions, setContainer} = createCodeMirror({
+  const {
+    view,
+    setOptions,
+    setContainer
+  } = createCodeMirror({
     container: editor,
-    ...computedProps
+    ...options
   });
 
   createEffect(() => setContainer(editor));
-  createEffect(() => setOptions(computedProps as any));
+
+  createEffect(() => setOptions(options as unknown))
+
   onCleanup(() => view()?.destroy());
 
   return (
     <div
-      ref={editor}
-      class={`solid-cm ${
-        pickedProps.className ? ` ${pickedProps.className}` : ''
-      }`}
-      {...others}
+      ref={ref => editor = ref}
+      class={`solid-cm ${options?.className ? ` ${options.className}` : ''}`}
+      {...htmlAttrs}
     />
   );
 };
