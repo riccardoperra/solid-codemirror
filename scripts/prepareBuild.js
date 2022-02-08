814e6ab38affde9a5ba6aa525c7498088d1b7d56
riccardoperra <riccardo.perra@icloud.com>
2022-02-08 21:54:52 +0100

    chore: move to monorepo

--- scripts/prepareBuild.js
+++ scripts/prepareBuild.js
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-const packageJson = fs.readFileSync('./package.json', 'utf8');
+const packageJson = fs.readFileSync('./packages/solid-codemirror/package.json', 'utf8');
 const {scripts, ...other} = JSON.parse(packageJson);
-fs.writeFileSync('./dist/package.json', JSON.stringify(other, null, 2));
-fs.copyFileSync('./README.md', './dist/README.md');
+fs.writeFileSync('./packages/solid-codemirror/dist/package.json', JSON.stringify(other, null, 2));
+fs.copyFileSync('./README.md', './packages/solid-codemirror/dist/README.md');
