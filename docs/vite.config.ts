ca329564827b73cbf1b3f70df64e53e8e68311be
riccardoperra <riccardo.perra@icloud.com>
2022-07-05 21:14:25 +0200

    docs: add solid start vercel

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
-import solid from "solid-start";
+import { defineConfig } from 'vite';
+import solid from 'solid-start';
 
 export default defineConfig({
   plugins: [
     {
-      ...(await import("@mdx-js/rollup")).default({
+      ...(await import('@mdx-js/rollup')).default({
         jsx: true,
-        jsxImportSource: "solid-js",
-        providerImportSource: "solid-mdx",
+        jsxImportSource: 'solid-js',
+        providerImportSource: 'solid-mdx',
       }),
-      enforce: "pre",
+      enforce: 'pre',
     },
-    solid({ ssr: false, extensions: [".mdx", ".md"] }),
+    solid({
+      ssr: false,
+      extensions: ['.mdx', '.md'],
+    }),
   ],
 });
